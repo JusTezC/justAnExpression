02f4b2a17562d261c4698be4886a7d87c796bc6a
Tech Geek <martezjmartez@outlook.com>
2025-08-20 13:33:19 -0500

    SERVER CHANGES

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const app = require('./app')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/todo-backend')
+mongoose.connect(process.env.MONGO_ADDRESS)
     .then(() => {
         app.listen(3000, () => {
             console.log("MONGODB CONNECTED.")
